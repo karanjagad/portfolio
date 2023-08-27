5d149b328578f932286c3e99d61c6e543fe85df1
karan <karanjagad@uni-koblenz.de>
2023-08-27 23:56:27 +0200

    init react js frontent

--- portfolio/api/urls.py
+++ portfolio/api/urls.py
@@ -1,8 +1,14 @@
 
+# from django.urls import path
+# from django.urls import include
+# from .views import ContactView
+
+# urlpatterns = [
+#     path('home', ContactView.as_view()),
+# ]
 from django.urls import path
-from django.urls import include
-from .views import main
+from .views import ContactView
 
 urlpatterns = [
-    path('', main),
+    path('home/', ContactView, name='home'),
 ]
--- portfolio/api/views.py
+++ portfolio/api/views.py
@@ -1,7 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-# Create your views here.
-
-def main(request):
-    return HttpResponse('Hello World')
+def ContactView(request):
+    return render(request, 'api/home.html')  # Make sure this path is correct
